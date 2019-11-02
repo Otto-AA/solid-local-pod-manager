10d2f97a0173efa7090c148f733cf4cac41c7883
A_A <21040751+Otto-AA@users.noreply.github.com>
2019-11-02 19:32:07 +0100

    Initial version

---
+++ src/LocalPodManagerUI/script.js
@@ -0,0 +1,120 @@
+/** @type {Pod[]} */
+let pods = []
+
+async function main() {
+    try {
+        updatePods(await fetchPods())
+        
+        const form = document.getElementById('add_pod')
+        form.onsubmit = e => {
+            e.preventDefault()
+            console.log('submit', e)
+            const name = document.getElementById('pod_name').value
+            const port = document.getElementById('port_number').value
+            const basePath = document.getElementById('base_path').value
+
+            addPod(new Pod(name, port, basePath, true))
+        }
+    } catch (err) {
+        console.error(err)
+        alert('Unexpected Error: ' + JSON.stringify(err))
+    }
+}
+
+async function fetchPods() {
+    const res = await fetch('/get_pods')
+    if (!res.ok) {
+        throw new Error(res)
+    }
+    const data = await res.json()
+    return data
+}
+
+function updatePods(fetchedPods) {
+    pods = []
+    fetchedPods.forEach(({ name, port, basePath, isActive }) => {
+        pods.push(new Pod(name, port, basePath, isActive))
+    })
+    updateDisplay()
+}
+
+function updateDisplay() {
+    if (!('content' in document.createElement('template'))) {
+        alert('This webpage is is not supported by your browser')
+        throw new Error('html template support required')
+    }
+    // Instantiate the table with the existing HTML tbody
+    // and the row with the template
+    const template = document.querySelector('#pod_entry')
+    const tbody = document.querySelector("tbody")
+    tbody.innerHTML = ''
+
+    // Clone the new row and insert it into the table
+    console.log(pods)
+    pods.forEach(({ name, port, basePath, isActive }) => {
+        const clone = document.importNode(template.content, true)
+        const td = clone.querySelectorAll("td")
+        td[0].textContent = name
+        td[1].innerHTML = `<a target="_blank" href="https://localhost:${port}/">${port}</a>`
+        td[2].textContent = basePath
+        td[3].textContent = isActive ? 'Active' : 'Disabled'
+        tbody.appendChild(clone);
+
+
+        console.log(td[4].querySelectorAll('button'))
+        const toggleButton = td[4].querySelectorAll('button')[0]
+        const deleteButton = td[4].querySelectorAll('button')[1]
+        toggleButton.addEventListener('click', e => setStatus(name, !isActive))
+        deleteButton.addEventListener('click', e => deletePod(name))
+    })
+}
+
+async function setStatus(name, status) {
+    const endpoint = status ? '/activate_pod' : '/deactivate_pod'
+    const res = await fetch(endpoint, {
+        method: 'POST',
+        body: JSON.stringify({ name }),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+    if (!res.ok) {
+        throw new Error(res)
+    }
+    pods.forEach(pod => {
+        if (pod.name === name)
+            pod.isActive = status
+    })
+    updateDisplay()
+}
+
+function deletePod(name) {
+    pods = pods.filter(pod => pod.name !== name)
+    updateDisplay()
+}
+
+async function addPod(pod) {
+    const res = await fetch('/add_pod', {
+        method: 'POST',
+        body: JSON.stringify(pod),
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+    if (!res.ok) {
+        throw new Error(res)
+    }
+    pods.unshift(pod)
+    updateDisplay()
+}
+
+class Pod {
+    constructor(name, port, basePath, isActive) {
+        this.name = name
+        this.port = port
+        this.basePath = basePath
+        this.isActive = isActive
+    }
+}
+
+main()
--- src/index.js
+++ src/index.js
@@ -1,28 +1,139 @@
-const httpsLocalhost = require("https-localhost")()
-const LocalPod = require('./LocalPod')
-const SolidRest = require('solid-rest')
-const FileHandler = require('solid-rest/src/file')
-const solidRest = new SolidRest([new FileHandler])
-const solidFileFetch = require('./solidFileFetch')
+const express = require('express')
+const LocalPod = require('solid-local-pod')
+const fetch = require('solid-local-pod/src/solidFileFetch')
+const httpsLocalhost = require('https-localhost')()
+const nconf = require('nconf')
 
-async function main() {
-    // TODO: Check if there is a better way to run localhost via https
-    const certs = await httpsLocalhost.getCerts()
-    const fetch = solidRest.fetch.bind(solidRest)
-
-    new LocalPod({
-        port: 3000,
-        basePath: '/home/a_a/test',
-        certs,
-        fetch: solidFileFetch
-    }).startListening()
-
-    new LocalPod({
-        port: 4000,
-        basePath: '/home/a_a/test',
-        certs,
-        fetch,
-        prefix: 'file://'
-    }).startListening()
+const port = 2700
+let cert = null
+const getCerts = async () => {
+    if (!cert)
+        cert = await httpsLocalhost.getCerts()
+    return cert
 }
-main()
+
+const app = express()
+app.use(express.static(__dirname + '/LocalPodManagerUI'))
+app.use(verifyHost, express.json())
+app.listen(port, () => {
+    console.log('Listening on port ' + port)
+})
+
+
+/** @type {Object.<string, LocalPod>} */
+const pods = {}
+nconf.use('file', { file: './pods.json' })
+nconf.defaults({ pods: {} })
+nconf.load(() => initPods())
+
+
+
+app.get('/get_pods', (req, res) => {
+    const responsePods = []
+    for (const [name, pod] of Object.entries(pods))
+        responsePods.push({
+            name,
+            port: pod.port,
+            basePath: pod.basePath,
+            isActive: pod.isListening()
+        })
+    res.status(200).json(responsePods)
+})
+app.post('/deactivate_pod', (req, res, next) => {
+    const { name } = req.body
+    pods[name].stopListening()
+
+    res.status(200).send()
+
+    updateStorage()
+})
+
+app.post('/activate_pod', (req, res, next) => {
+    const { name } = req.body
+    pods[name].startListening()
+
+    res.status(200).send()
+
+    updateStorage()
+})
+
+app.post('/delete_pod', (req, res, next) => {
+    if (pods[name].isListening())
+        pods[name].stopListening()
+
+    delete pods[name]
+    res.status(200).send()
+
+    updateStorage()
+})
+
+app.post('/add_pod', async (req, res, next) => {
+    const config = req.body
+    const pod = await createPod(config)
+    pods[config.name] = pod
+    pod.startListening()
+    res.status(200).send()
+
+    updateStorage()
+})
+
+function verifyHost(req, res, next) {
+    if (req.headers.host !== `localhost:${port}`) {
+        return res.status(403).send('Invalid host')
+    }
+    return next()
+}
+
+async function createPod({ name, port, basePath }) {
+    const certs = await getCerts()
+    return new LocalPod({
+        port,
+        basePath,
+        certs,
+        fetch
+    })
+}
+
+async function initPods() {
+    await Promise.all(Object.entries(nconf.get('pods')).map(async ([name, pod]) => {
+        const config = {
+            name,
+            port: pod.port,
+            basePath: pod.basePath
+        }
+        pods[name] = await createPod(config)
+        if (pod.isActive)
+            pods[name].startListening()
+    }))
+    console.log('Finished loading')
+    console.log(nconf.get('pods'))
+}
+
+async function updateStorage() {
+    //
+    // Save the configuration object to disk
+    //
+    nconf.set('pods', serializePods(pods))
+    return new Promise((resolve, reject) => {
+        nconf.save(function (err) {
+            if (err) {
+                console.error(err.message);
+                return;
+            }
+            console.log('Configuration saved successfully.');
+        });
+    })
+}
+
+function serializePods(pods) {
+    const serialized = {}
+    for (const [name, pod] of Object.entries(pods)) {
+        serialized[name] = {
+            port: pod.port,
+            basePath: pod.basePath,
+            isActive: pod.isListening()
+        }
+    }
+    console.log('serialized', serialized)
+    return serialized
+}
